a45db02adb5000931e6d436e4ca62b9280b357eb
Asif Al Rumel <157966985+asifff009@users.noreply.github.com>
2025-07-28 09:41:05 +0600

    updated post job php connection with db

--- 4_postJob.php
+++ 4_postJob.php
@@ -8,19 +8,16 @@ $experience = $_GET['c'];
 $duration = $_GET['d'];
 $location = $_GET['e'];
 $contact = $_GET['f'];
-
-
+$email = $_GET['g']; 
 
 if(mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-else{
+} else {
     echo "Connected successfully to MySQL database.";
 }
 
-$sql = "INSERT INTO jobs(title, description, experience, duration, location, 
-contact) VALUES('$title','$description','$experience','$duration','$location',
-'$contact')";
+$sql = "INSERT INTO jobs(title, description, experience, duration, location, contact, email) 
+        VALUES('$title','$description','$experience','$duration','$location','$contact', '$email')";
 $result = mysqli_query($con, $sql);
 
 if($result) {
@@ -28,6 +25,4 @@ if($result) {
 } else {
     echo "Error inserting data: " . mysqli_error($con);
 }
-
-
 ?>
